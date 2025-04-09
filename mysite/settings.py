b4c430704ea037f8efd2f9afa660156575d75f59
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-09 09:46:33 -0600

    added static file finder

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,7 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_FINDERS = [
+    'compressor.finders.CompressorFinder'
 
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
