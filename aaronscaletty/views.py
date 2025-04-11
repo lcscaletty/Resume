ffb92c14df6cf90e79722920cebce23eea35e177
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-10 20:09:34 -0600

    fix security issue

--- aaronscaletty/views.py
+++ aaronscaletty/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import loader
 from django.shortcuts import get_object_or_404
 import pandas as pd
 # Create your views here.
-df= pd.read_csv('/home/ascaletty23/projects/python/resume_scanner/resume.csv')
-i=0
-while i < 4
-    df['Title']['Student Activities and Involvement i']= "activity" + i
-    i= i+1
-j=0
-while j <4
 
+import numpy as np
+df= pd.read_csv('/home/ascaletty23/projects/python/resume_scanner/resume.csv')
+print(df)
+activity= np.empty((8,1), dtype= object)
+i=0
+while i < 8:
+    activity[i]= df.iloc[i,1]
+    i+=1
 def home(request):
     return render(request, "aaronscaletty/home.html" )
 
