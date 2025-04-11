4102656e3a1bd9e87a3e1b6a8b1a137225f36b4a
Aaron Scaletty <ascaletty23@gmail.com>
2025-04-10 19:57:10 -0600

    fixed security issue

--- aaronscaletty/views.py
+++ aaronscaletty/views.py
@@ -2,10 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import loader
 from django.shortcuts import get_object_or_404
-
+import pandas as pd
 # Create your views here.
+df= pd.read_csv('/home/ascaletty23/projects/python/resume_scanner/resume.csv')
+i=0
+while i < 4
+    df['Title']['Student Activities and Involvement i']= "activity" + i
+    i= i+1
+j=0
+while j <4
+
 def home(request):
-    message= "hi folks"
     return render(request, "aaronscaletty/home.html" )
 
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os.path
+import os 
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pzdnwqhv7r8f9fj%=e0m_&((9f)8&^wc@=$x6ar!pmsc*+w_=a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
